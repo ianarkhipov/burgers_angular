7f10dce45a1eab4eb52a30c9f3dd3177603a72ac
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2023-12-14 15:11:28 +0300

    Sucsess on autofill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -126,8 +126,11 @@ export class AppComponent {
   }
 
 
-  scrollTo(target: HTMLElement ) {
+  scrollTo(target: HTMLElement, burger?: any) {
     target.scrollIntoView({behavior: "smooth"});
+    if (burger) {
+      this.form.patchValue({order: burger.title + ' (' + burger.price + ' ' + this.currency + ')'});
+    }
   }
 
   confirmOrder() {
